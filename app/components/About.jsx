1b78e2c47d2e511f664a59f3840cd9a19c61f484
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-08-30 17:33:52 +0530

    style added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,11 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About page.</p>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application built on react. I have built it for complete react web app developer course.</p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a>- This is the javascrit libarary which is used.</li>
+      </ul>
     </div>
   );
 }
