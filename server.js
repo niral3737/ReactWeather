fb80f51e0b0ffd05734fd215046531dc683a4fec
niral.eagle100@gmail.com <niral.eagle100@gmail.com>
2016-08-30 12:47:37 +0530

    Set up for heroku

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
+const PORT = process.env.PORT || 3000;
 
 //create app
 var app = express();
 
+app.use(function (req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express app is up and running');
+app.listen(PORT, function () {
+  console.log(`Express app is up and running on ${PORT}`);
 })
